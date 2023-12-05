7750b6af195596999256bd7c59a760ac7afd9dd7
Cass Dahle <isaacbd.personal@gmail.com>
2023-12-04 23:23:37 -0800

    started on handle_response_request

--- proxy.c
+++ proxy.c
@@ -5,22 +5,20 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
-// a struct for the values returned from forward_request
-typedef struct client_request_data{
-
-} client_request_data;
-
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
+
+/* -------------------------------------------------------------------------- */
+/*                                    main                                    */
+/* -------------------------------------------------------------------------- */
 
 int main(int argc, char **argv) {
   int listenfd, connfd;
   socklen_t clientlen;
   struct sockaddr_storage clientaddr; /* Enough space for any address */
   char client_hostname[MAXLINE], client_port[MAXLINE];
-  client_request_data* request_data;
 
   if (argc != 2) {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
@@ -28,11 +26,9 @@ int main(int argc, char **argv) {
   }
 
   // opens a lisening (looking for new conenctions) port to argv[1]
-  listenfd = Open_listenfd(port);
+  listenfd = Open_listenfd(argv[1]);
 
-/* -------------------------------------------------------------------------- */
-/*        loop to constantly search for connections and handle requests       */
-/* -------------------------------------------------------------------------- */
+/* ------ loop to constantly search for connections and handle requests ----- */
   while (1) {
     clientlen = sizeof(struct sockaddr_storage);
 
@@ -45,11 +41,8 @@ int main(int argc, char **argv) {
     // print the connection
     printf("Connected to (%s, %s)\n", client_hostname, client_port);
 
-    // parse the request sent from the web browser
-    reqest_data = forward_request(connfd);    
-
-    // send the response from the server back to the web browser
-    forward_response(request_data);
+    // handle one client/server itneraction
+    handle_request_response(connfd);
 
     // closes the Accepted socket
     Close(connfd);
@@ -58,12 +51,75 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-// get the request from the web browser and forward the request to hte endpoint server
-client_request_data* forward_request(int connfd){
+/* -------------------------------------------------------------------------- */
+/*                  handle_request_response helper functions                  */
+/* -------------------------------------------------------------------------- */
+
+// returns an error message to hte client
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg) 
+{
+    char buf[MAXLINE];
+
+    /* Print the HTTP response headers */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+
+    /* Print the HTTP response body */
+    sprintf(buf, "<html><title>Tiny Error</title>");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<body bgcolor=""ffffff"">\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "%s: %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<p>%s: %s\r\n", longmsg, cause);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<hr><em>The Tiny Web server</em>\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+}
+
+// reads the request header
+void read_requesthdrs(rio_t *rp) 
+{
+    char buf[MAXLINE];
+
+    Rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+    while(strcmp(buf, "\r\n")) {          //line:netp:readhdrs:checkterm
+	    Rio_readlineb(rp, buf, MAXLINE);
+	    printf("%s", buf);
+    }
+    return;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                           handle_request_response                          */
+/* -------------------------------------------------------------------------- */
+
+// handle one client/server interaction
+void handle_request_response(int connfd){
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE], cgiargs[MAXLINE];
+    rio_t rio;
+
+    /* ---------------------- Read request line and headers --------------------- */
+    Rio_readinitb(&rio, connfd);
+    if (!Rio_readlineb(&rio, buf, MAXLINE)){  //line:netp:doit:readrequest
+      return;
+    }
+    printf("%s", buf);
+    sscanf(buf, "%s %s %s", method, uri, version);       //line:netp:doit:parserequest
+    if (strcasecmp(method, "GET")) {                     //line:netp:doit:beginrequesterr
+      clienterror(connfd, method, "501", "Not Implemented","Tiny does not implement this method");
+      return;
+    }                                                    //line:netp:doit:endrequesterr
+    read_requesthdrs(&rio);                              //line:netp:doit:readrequesthdrs
+
+
+
 
 }
 
-// get the response from the endpoint server and forward the resopnse to the web browser
-void forward_response(client_request_data* request_data){
-
-}
